d61cf492811e1d01562338a4a944de37ced81a77
Neha <nehanupur497@gmail.com>
2023-04-30 16:15:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<!-- Weather App -->
+
+<p>This is a simple weather application that allows users to enter a city name as input and get the current weather condition of the entered city.</p>
+
+<!-- Technologies -->
+
+<h2>Technologies</h2>
+<ul>
+    <li>HTML5</li>
+    <li>CSS3</li>
+    <li>JavaScript</li>
+    <li>Node.js</li>
+    <li>Express.js</li>
+    <li>APIs:
+        <ul>
+            <li>OpenWeatherMap API</li>
+            <li>OpenWeatherMap Geocoding API</li>
+        </ul>
+    </li>
+</ul>
+
+<!-- Setup -->
+
+<h2>Setup</h2>
+<p>To set up and run the project, follow these steps:</p>
+<ol>
+    <li>Clone this repository to your local machine.</li>
+    <li>In the project directory, run the following command to install the required dependencies:</li>
+    <pre><code>npm install</code></pre>
+    <li>Rename the `.env.example` file to `.env` and replace the `API_KEY` value with your OpenWeatherMap API key.</li>
+    <li>Run the following command to start the server:</li>
+    <pre><code>node app.js</code></pre>
+    <li>Open your browser and enter `http://localhost:3000` in the address bar to view the application.</li>
+</ol>
+
+<!-- Usage -->
+
+<h2>Usage</h2>
+<p>To use the application, follow these steps:</p>
+<ol>
+    <li>Enter the name of a city in the input field.</li>
+    <li>Click the "Get Weather" button to see the current weather condition of the entered city.</li>
+</ol>
+
+<!-- Screenshots -->
+
+<h2>Screenshots</h2>
+<img src="image/Weather 1.png" alt="Screenshot 1">
+<br/>
+<img src="image/Weather 2.png" alt="Screenshot 2">
+
+
---
+++ app.js
@@ -0,0 +1,141 @@
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+ 
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "74fd7ba8041e758c679959b5cad006f7";
+    const unit = "metric";
+
+    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`;
+    https.get(geoUrl, function(response){
+
+        response.on("data", function(data){
+            const locationData = JSON.parse(data);
+            const lat = locationData[0].lat;
+            const lon = locationData[0].lon;
+        
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+
+            https.get(weatherUrl, function(response){
+
+                response.on("data", function(data){
+                    const weatherData = JSON.parse(data);
+                    const temp = weatherData.main.temp.toFixed(1);
+                    const weatherDescription = weatherData.weather[0].description;
+                    const icon = weatherData.weather[0].icon;
+                    const imageURL = `https://openweathermap.org/img/wn/${icon}.png`;
+                    const temperatureColor = getTemperatureColor(temp);
+
+                    res.write(`<!DOCTYPE html>
+                    <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Weather Report</title>
+                        <style>
+                            body {
+                                margin: 0;
+                                font-family: Arial, sans-serif;
+                                background-color: #F5EBEB;
+                                background-image: url("https://img.freepik.com/free-photo/sunny-blue-sky_1048-8145.jpg?w=900&t=st=1682848304~exp=1682848904~hmac=5f6a0a25662cb6d9148ef732efe14acba669177097adfb2414baf28af9f67a6a");
+                                background-size: cover;
+                                background-position: center center;
+                                background-repeat: no-repeat;
+                            }
+                            .container {
+                                display: flex;
+                                flex-direction: column;
+                                justify-content: center;
+                                align-items: center;
+                                min-height: 100vh;
+                            }
+                            .weather-card {
+                                display: flex;
+                                flex-direction: column;
+                                width: 500px;
+                                background-color: white;
+                                border-radius: 10px;
+                                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+                                overflow: hidden;
+                            }
+                            .location {
+                                padding: 20px;
+                                background-color: ${temperatureColor};
+                            }
+                            .location h1 {
+                                font-size: 48px;
+                                color: white;
+                                margin: 0;
+                            }
+                            .temperature {
+                                display: flex;
+                                align-items: center;
+                                justify-content: center;
+                                font-size: 96px;
+                                font-weight: bold;
+                                margin: 20px;
+                                color: ${temperatureColor};
+                            }
+                            .description {
+                                text-align: center;
+                                font-size: 24px;
+                                margin: 20px;
+                            }
+                            .icon {
+                                align-self: center;
+                                margin: 20px;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="container">
+                            <div class="weather-card">
+                                <div class="location">
+                                    <h1>${query}</h1>
+                                </div>
+                                <div class="temperature">${temp}&deg;C</div>
+                                <div class="description">${weatherDescription}</div>
+                                <img class="icon" src="${imageURL}" alt="Weather Icon">
+                            </div>
+                        </div>
+                    </body>
+                    </html>`);
+                    res.send();
+                });
+
+            });
+
+        });
+        
+    });
+    
+});
+
+function getTemperatureColor(temp) {
+    const temperature = parseFloat(temp);
+    if (temperature >= 35) {
+        return "#cf0000";
+    } else if (temperature >= 25) {
+        return "#ff8c00";
+    } else if (temperature >= 15) {
+        return "#ffd700";
+    } else if (temperature >= 5) {
+        return "#87cefa";
+    } else {
+        return "#add8e6";
+    }
+}
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+});
